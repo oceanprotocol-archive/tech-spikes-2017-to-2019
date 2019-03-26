e53d66557ea7a8f60b6de5f89d101e41fe8f0e82
gongf05 <fang.ucla@gmail.com>
2019-03-25 17:03:53 -0700

    remove external adapter folder

--- 6-random-number/chainlink/external_adapter/3_oracle_migration.js
+++
@@ -1,7 +0,0 @@
-var oracle = artifacts.require("Oracle");
-
-const linkToken = '0xa36085F69e2889c224210F603D836748e7dC0088'
-
-module.exports = (deployer, network, accounts) => {
-  deployer.deploy(oracle, linkToken, {from: accounts[0]});
-};
--- 6-random-number/chainlink/external_adapter/4_requestGCP_migration.js
+++
@@ -1,7 +0,0 @@
-var Oracle = artifacts.require("Oracle");
-var GcpService = artifacts.require("requestGCP");
-
-module.exports = (deployer, network, accounts) => {
-  const oracleAddress = Oracle.address
-  deployer.deploy(GcpService, oracleAddress, {from: accounts[0]});
-};
