bfdeffb63cee3db7ae6c4f85e163020504823a64
arsenyjin <kinspotting@protonmail.com>
2020-03-13 10:25:47 +0100

    innit coin contract

---
+++ 23-ocean-v3/poc2/contracts/Coin.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol';
+
+contract Coin is ERC20, ERC20Mintable {
+
+	
+}
