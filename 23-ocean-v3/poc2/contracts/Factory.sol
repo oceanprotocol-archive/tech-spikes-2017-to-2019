aa76c2780ba75004a385ea3b5de62b8ff0a1c735
arsenyjin <kinspotting@protonmail.com>
2020-03-16 17:04:54 +0100

    add erc20

--- 23-ocean-v3/poc2/contracts/Factory.sol
+++ 23-ocean-v3/poc2/contracts/Factory.sol
@@ -1,9 +1,19 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 import "@optionality.io/clone-factory/contracts/CloneFactory.sol";
-// import "@openzeppelin/contracts/ownership/Ownable.sol";
-
 
 contract CoinFactory is CloneFactory {
+
+	address public TokenTemplate;
+
+	constructor(address _template) public {
+    	TokenTemplate = _template;
+  	}
+
+	function createMinimalToken() external returns(address  minimalToken) {
+		minimalToken = createClone(TokenTemplate);
+	}
+
+	
 
 }
