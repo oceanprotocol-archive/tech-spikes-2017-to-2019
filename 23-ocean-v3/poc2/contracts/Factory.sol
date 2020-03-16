f3895e0273bd177d5c149fbc76b66cca580a93c7
arsenyjin <kinspotting@protonmail.com>
2020-03-16 13:34:29 +0100

    add CloneFactory

---
+++ 23-ocean-v3/poc2/contracts/Factory.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+import "@optionality.io/clone-factory/contracts/CloneFactory.sol";
+// import "@openzeppelin/contracts/ownership/Ownable.sol";
+
+
+contract CoinFactory is CloneFactory {
+
+}
