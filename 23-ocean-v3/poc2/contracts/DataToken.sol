0fd6dbb09b6694bee9bb135f8a78a3405bf36ceb
arsenyjin <kinspotting@protonmail.com>
2020-03-18 11:32:56 +0100

    add mint + cut fee

--- 23-ocean-v3/poc2/contracts/DataToken.sol
+++ 23-ocean-v3/poc2/contracts/DataToken.sol
@@ -4,6 +4,11 @@ import './common/ERC20.sol';
 
 contract DataToken is ERC20 {
 
+    event DeductedFee(
+        address from,
+        uint256 value
+    );
+
     string  name;
     string  symbol;
     uint8   decimals;
@@ -27,4 +32,35 @@ contract DataToken is ERC20 {
 		metadata = _metadata;
 		owner 	 = msg.sender;
 	}
+
+    function mint(address to)
+        public
+        payable
+    {
+    	require(owner == msg.sender,
+    		"minter should be a message sender");
+        uint256 startGas = gasleft();
+        super._mint(address(this), 1);
+        uint256 usedGas = startGas - gasleft();
+        uint256 fee = usedGas * tx.gasprice;
+        require(
+            _deductFee(fee, msg.sender),
+            'failed to deduct ocean fee'
+        );
+        _transfer(address(this), to, 1);
+    }
+
+    function _deductFee(uint256 fee, address from)
+        private
+        returns (bool)
+    {
+    	require(msg.value == fee,
+    		"ether sent is not enough");
+        emit DeductedFee(
+            from,
+            fee
+        );
+        return true;
+    }
 }
+
