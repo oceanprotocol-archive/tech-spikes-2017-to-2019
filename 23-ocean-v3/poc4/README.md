9b0d8e070b091eda921d9df3f2479972ca3bb10c
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2020-04-16 12:53:29 +0200

    add poc-4

---
+++ 23-ocean-v3/poc4/README.md
@@ -0,0 +1,14 @@
+# PoC 4 
+
+## Architecture
+
+Find below the high level architecture 
+
+![](docs/imgs/Protocol-v3-revisit.png)
+
+## Getting started
+
+TBD
+
+## Testing
+TBD
---
+++ 23-ocean-v3/poc4/contracts/TokenFactory.sol
@@ -0,0 +1,80 @@
+pragma solidity 0.6.0;
+
+import './Deployer.sol';
+
+contract TokenFactory is Deployer {
+    
+    address public tokenTemplate;
+    address public currentTokenAddress;
+    
+    event TokenCreated(
+        address indexed newTokenAddress, 
+        address indexed templateAddress,
+        string indexed name
+    );
+    
+    event TokenRemoved(
+        address indexed tokenAddress,
+        address indexed templateAddress,
+        address indexed removedBy
+    );
+    
+    constructor (
+        address _template,
+        address _registry
+    ) 
+        public 
+    {
+        require(
+            _template != address(0) &&
+            _registry != address(0),
+            'Invalid TokenFactory initialization'
+        );
+        tokenTemplate = _template;
+        // create tokenRegistry instance 
+    }
+    
+    // equals publish new asset 
+    function createToken(
+        string memory _logic,
+        string memory _name, 
+        uint256 _value
+    ) 
+        public
+        returns (address token)
+    {
+        bytes memory _initPayload  = abi.encodeWithSignature(_logic, _name, _value);
+        token = deploy(tokenTemplate);
+        
+        require(
+          token != address(0),
+          'Failed to perform minimal deploy of a new token'
+        );
+        
+        // init Token
+        token.call(_initPayload);
+        //TODO: store Token in Token Registry
+        currentTokenAddress = token;
+        //TODO: fix ownership and access control
+        // set Token Owner to msg.sender
+        emit TokenCreated(
+            token, 
+            tokenTemplate,
+            _name
+        );
+    }
+    
+    
+    function removeToken(
+        address _token
+    ) 
+        public 
+        //onlyTokenOwner(_token)
+    {
+        emit TokenRemoved(
+            _token,
+            tokenTemplate,
+            msg.sender
+        );
+    }
+}
